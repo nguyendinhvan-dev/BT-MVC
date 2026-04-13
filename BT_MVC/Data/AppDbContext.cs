9435460bdaaec7388fd61872afe3624a70782c7b
NGuyenVAn21 <dinhvanparkour@gmail.com>
2026-04-13 14:34:36 +0700

    Add DbContext

---
+++ BT_MVC/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿namespace BT_MVC.Data
+{
+    using BT_MVC.Models;
+    using Microsoft.EntityFrameworkCore;
+
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+    }
+}
