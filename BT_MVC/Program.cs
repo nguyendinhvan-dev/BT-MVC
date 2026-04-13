d6f8a5023cb40d62df42d88f64da6ead2e1311bf
NGuyenVAn21 <dinhvanparkour@gmail.com>
2026-04-13 14:42:19 +0700

    Fix DbContext configuration and connection string

--- BT_MVC/Program.cs
+++ BT_MVC/Program.cs
@@ -1,7 +1,15 @@
 using BT_MVC.Data;
 using Microsoft.EntityFrameworkCore;
 
+
+
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
